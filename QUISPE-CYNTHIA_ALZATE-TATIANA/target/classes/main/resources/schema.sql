aee6add794d5e3463800d684a74d9c3dd092594b
tatiana <tuti.alzate@gmail.com>
2024-06-20 20:28:22 -0400

    Eliminar archivo_innecesario.txt

--- QUISPE-CYNTHIA_ALZATE-TATIANA/target/classes/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS odontologo (
-                                          id INT AUTO_INCREMENT PRIMARY KEY,
-                                          numero_matricula VARCHAR(255) NOT NULL,
-    nombre VARCHAR(255) NOT NULL,
-    apellido VARCHAR(255) NOT NULL
-    );
